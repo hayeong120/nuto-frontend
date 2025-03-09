bcc1b4285d06c53fb6150c65e657af55b0be70e0
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-03-09 22:11:04 +0900

    스티커 추가 기능

--- src/pages/EditPost.tsx
+++ src/pages/EditPost.tsx
@@ -6,16 +6,41 @@ import Footer from "../components/Footer";
 import style from "../styles/EditPost.module.css";
 import * as fabric from "fabric";
 
+fabric.Object.prototype.transparentCorners = false;
+fabric.Object.prototype.cornerColor = "blue";
+fabric.Object.prototype.cornerStyle = "circle";
+
 function EditPost() {
   const { image } = useImage();
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(null);
 
+  const stickers = [
+    "/images/물주전자.png",
+    "/images/꽃.png",
+    "/images/해.png",
+    "/images/구름캐릭.png",
+    "/images/풀떼기.png",
+    "/images/무당벌레.png",
+    "/images/나무.png",
+    "/images/아이디어.png",
+  ];
+
+  const deleteIcon = "/images/Delete.png";
+  const deleteImg = document.createElement("img");
+  deleteImg.src = deleteIcon;
+  const sizeIcon = "/images/Size.png";
+  const sizeImg = document.createElement("img");
+  sizeImg.src = sizeIcon;
+  const translateIcon = "/images/Translate.png";
+  var translateImg = document.createElement("img");
+  translateImg.src = translateIcon;
+
   useEffect(() => {
     if (!canvasRef.current) return;
 
     const newCanvas = new fabric.Canvas(canvasRef.current, {
-      width: 343,
+      width: 358,
       height: 450,
     });
 
@@ -23,7 +48,7 @@ function EditPost() {
       left: 0,
       top: 0,
       fill: "white",
-      width: 343,
+      width: 358,
       height: 450,
       selectable: false,
     });
@@ -35,20 +60,29 @@ function EditPost() {
       img.src = image;
 
       img.onload = () => {
+        const scaleX = 314 / img.width;
+        const scaleY = 314 / img.height;
+
         const imgObj = new fabric.Image(img, {
           left: 22,
           top: 22,
           selectable: false,
         });
 
-        const scaleX = 300 / img.width;
-        const scaleY = 300 / img.height;
-
         imgObj.set({
           scaleX: scaleX,
           scaleY: scaleY,
           width: img.width,
           height: img.height,
+        });
+
+        imgObj.controls.deleteControl = new fabric.Control({
+          x: 0.5,
+          y: -0.5,
+          offsetY: 16,
+          cursorStyle: "pointer",
+          mouseUpHandler: deleteObject,
+          render: renderIcon,
         });
 
         newCanvas.add(imgObj);
@@ -63,12 +97,68 @@ function EditPost() {
     };
   }, [image]);
 
+  const add = (idx: number) => {
+    const stickerImg = new Image();
+    stickerImg.src = stickers[idx];
+    stickerImg.onload = () => {
+      const scaleX = 70 / stickerImg.width;
+      const scaleY = 70 / stickerImg.height;
+      if (fabricCanvas) {
+        const imgObj = new fabric.Image(stickerImg, {
+          left: 50,
+          top: 50,
+          selectable: true,
+        });
+
+        imgObj.set({
+          scaleX: scaleX,
+          scaleY: scaleY,
+          width: stickerImg.width,
+          height: stickerImg.height,
+        });
+
+        fabricCanvas.add(imgObj);
+        fabricCanvas.renderAll();
+      }
+    };
+  };
+
+  function deleteObject(_eventData: any, transform: any) {
+    const canvas = transform.target.canvas;
+    canvas.remove(transform.target);
+    canvas.requestRenderAll();
+  }
+
+  function renderIcon(
+    ctx: any,
+    left: any,
+    top: any,
+    _styleOverride: any,
+    fabricObject: any
+  ) {
+    const size = 24;
+    ctx.save();
+    ctx.translate(left, top);
+    ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+    ctx.drawImage(deleteImg, -size / 2, -size / 2, size, size);
+    ctx.restore();
+  }
+
   return (
     <div className={def.Body}>
       <Header prevSrc="-1" nextSrc="/nuto" />
       <div className={style.EditPostContainer}>
         <p>폴라로이드를 취향에 맞게 꾸며주세요!</p>
         <canvas ref={canvasRef} id="canvas"></canvas>
+        <div className={style.stickerContainer}>
+          {stickers.map((sticker: string, idx: number) => {
+            return (
+              <button key={idx} onClick={() => add(idx)}>
+                <img src={sticker} alt={sticker} />
+              </button>
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </div>
