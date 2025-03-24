dd36a5d8e9de191325a80ebe50e78e2a57e816e1
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-03-24 20:50:46 +0900

    사용자 채팅 컴포넌트

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -5,12 +5,26 @@ import Members from "../components/Members";
 import { profiles } from "../assets/json/profiles";
 import style from "../styles/Chat.module.css";
 import DefaultChat from "../components/DefaultChat";
+import SendChat from "../components/SendChat";
+import Footer from "../components/Footer";
 
 function Chat({}) {
   const [profile, setProfile] = useState(profiles[0]);
+  const [message, setMessage] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
   const changeMember = (idx: number) => {
     setProfile(profiles[idx]);
   };
+
+  const inputedMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const sendMessage = () => {
+    setInputMessage(message);
+    setMessage("");
+  };
+
   return (
     <div className={def.Body}>
       <Members profiles={profiles} changeMember={changeMember} />
@@ -20,6 +34,24 @@ function Chat({}) {
         comment={profile.comment}
         img={profile.img}
       />
+
+      {inputMessage ? <SendChat comment={inputMessage} /> : ""}
+
+      <div className={style["message-container"]}>
+        <div className={style["input-container"]}>
+          <input
+            placeholder="텍스트 입력"
+            value={message}
+            onChange={inputedMessage}
+          />
+        </div>
+        <img
+          src="/images/sendButton.png"
+          alt="sendButton"
+          onClick={sendMessage}
+        />
+      </div>
+      <Footer />
     </div>
   );
 }
