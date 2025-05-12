c557cc896959f0c06e83c170e88c09205befc542
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-12 19:31:24 +0900

    post 에러 해결

--- src/pages/Mobile/Home.tsx
+++ src/pages/Mobile/Home.tsx
@@ -29,7 +29,7 @@ function Home() {
           `https://nuto.mirim-it-show.site/post`
         );
         console.log(response);
-        setPosts(response.data.statusCode === 500 ? [] : response.data.posts);
+        setPosts(response.data);
       } catch (err) {
         console.error(err);
       }
