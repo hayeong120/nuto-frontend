8cefd7b3694144f644b717230ee5d0c81cc91da7
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-24 14:13:03 +0900

    메시지 확인 페이지 오류 수정

--- src/pages/Mobile/Admin.tsx
+++ src/pages/Mobile/Admin.tsx
@@ -26,31 +26,34 @@ function Admin() {
     changeMember(idx);
   }, [profile, idx]);
 
-  const changeMember = useCallback(async (idx: number) => {
-    setProfile(profiles[idx]);
-    setIdx(idx);
-    try {
-      const response = await axios.get(
-        `https://nuto.mirim-it-show.site/message${profile.name}`
-      );
+  const changeMember = useCallback(
+    async (idx: number) => {
+      setProfile(profiles[idx]);
+      setIdx(idx);
+      try {
+        const response = await axios.get(
+          `https://nuto.mirim-it-show.site/message/${profile.name}`
+        );
 
-      const adminChats: adminChat[] = response.data.data.map(
-        (chat: { message: string; createdAt: string }) => {
-          return {
-            type: "admin-chat",
-            data: {
-              message: chat.message,
-              createdAt: chat.createdAt,
-            },
-          };
-        }
-      );
+        const adminChats: adminChat[] = response.data.data.map(
+          (chat: { message: string; createdAt: string }) => {
+            return {
+              type: "admin-chat",
+              data: {
+                message: chat.message,
+                createdAt: chat.createdAt,
+              },
+            };
+          }
+        );
 
-      setChattings(adminChats);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [profiles]);
+        setChattings(adminChats);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [profile.name]
+  );
 
   if (isLogin) {
     return (
