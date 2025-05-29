ab299c03fc949efa1351ead0461c3638190d1e68
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-29 19:39:05 +0900

    초기 로그인 수정

--- src/pages/Mobile/User.tsx
+++ src/pages/Mobile/User.tsx
@@ -1,41 +1,46 @@
 import { useNavigate } from "react-router-dom";
 import { usePostInfo } from "../../context/PostInfoContext";
-import styles from "../../styles/User.module.css"
+import styles from "../../styles/User.module.css";
 
 function User() {
-    const navigate = useNavigate();
-    const {name, setName, password, setPassword} = usePostInfo();
+  const navigate = useNavigate();
+  const { name, setName, password, setPassword } = usePostInfo();
 
-    const login = () => {
-        if(name != "" && password != ""){
-            navigate("/home")
-            console.log('클릭');
-        }
+  const login = () => {
+    if (name != "" && password != "") {
+      navigate("/home");
+      console.log("클릭");
     }
+  };
 
-    return (
-        <div className={styles.loginBody}>
-            <div className={styles.loginContainer}>
-                <img 
-                    src="/images/userTomato.svg"
-                    className={styles.tomato}/>
-                <img 
-                    src="/images/userNuto.svg"
-                    className={styles.nuto}/>
-                <input 
-                    placeholder="아이디"
-                    value={name}
-                    onChange={e => setName((e.target.value).trim())}
-                    className={styles.input}/>
-                <input 
-                    placeholder="비밀번호"
-                    value={password}
-                    onChange={e => setPassword((e.target.value).trim())}
-                    className={styles.input}/>
-                <button onClick={login} className={styles.loginBtn}>들어가기</button>
-            </div>
+  return (
+    <div className={styles.loginBody}>
+      <div className={styles.loginContainer}>
+        <img
+          src="/images/userTomato.svg"
+          className={styles.tomato}
+          alt="tomatoImage"
+        />
+        <div className={styles.explainContainer}>
+          <p className={styles.explainText}>
+            아이디를 바탕으로 포스트를 작성하거나
+          </p>
+          <p className={styles.explainText}>댓글을 작성할 수 있습니다!</p>
         </div>
-    )
+
+        <input
+          placeholder="아이디"
+          value={name}
+          onChange={(e) => setName(e.target.value.trim())}
+          className={styles.input}
+        />
+
+        <button onClick={login} className={styles.loginBtn}>
+          들어가기
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default User;
