d6568931a103926d6a6118754307146f6c082a50
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-29 18:52:58 +0900

    삭제 기능 리랜더링 추가

--- src/pages/Mobile/Home.tsx
+++ src/pages/Mobile/Home.tsx
@@ -13,7 +13,7 @@ interface Comment {
   message: string;
   createdAt: string;
 }
-interface PostProps {
+type PostType = {
   _id: string;
   name: string;
   polariodImage: string;
@@ -21,33 +21,33 @@ interface PostProps {
   location: string;
   password: string;
   comments: Comment[];
-}
+};
 
 function Home() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [posts, setPosts] = useState<PostType[]>([]);
   const [selectPost, setSelectPost] = useState<string | null>(null);
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const response = await axios.get(
-          `https://nuto.mirim-it-show.site/post`
-        );
-        // console.log(response);
-        setPosts(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  const fetchPost = async () => {
+    try {
+      const response = await axios.get(`https://nuto.mirim-it-show.site/post`);
+      console.log(response);
+      setPosts(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    getPosts();
+  useEffect(() => {
+    fetchPost();
   }, []);
 
   return (
     <div className={style.body}>
-      { selectPost && <ModalPortal>
-        <Comment postId={selectPost} setSelectPost={setSelectPost} />
-      </ModalPortal>}
+      {selectPost && (
+        <ModalPortal>
+          <Comment postId={selectPost} setSelectPost={setSelectPost} />
+        </ModalPortal>
+      )}
 
       <Helmet>
         <title>nuto home</title>
@@ -57,7 +57,14 @@ function Home() {
       {posts.length === 0 ? (
         <p>게시물이 없습니다.</p>
       ) : (
-        posts && posts.map((post) => <Post post={post} setSelectPost={setSelectPost} />)
+        posts &&
+        posts.map((post) => (
+          <Post
+            post={post}
+            refetchPost={fetchPost}
+            setSelectPost={setSelectPost}
+          />
+        ))
       )}
       <Footer />
     </div>
